7190ee69159dfa5221df1b96b1064aa8fcbb3fa4
Arielhandrade <arielhandrade@gmail.com>
2022-06-03 21:47:03 -0300

    carpeta src

---
+++ src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "u0xph1cigxy9mgfr",
+    "password": "yLSc59A1sdeajqFeMTY8",
+    "database": "budu6qyznxzudxuygolw",
+    "host": "budu6qyznxzudxuygolw-mysql.services.clever-cloud.com",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "u0xph1cigxy9mgfr",
+    "password": "yLSc59A1sdeajqFeMTY8",
+    "database": "budu6qyznxzudxuygolw",
+    "host": "budu6qyznxzudxuygolw-mysql.services.clever-cloud.com",
+    "dialect": "mysql"
+  }
+}
